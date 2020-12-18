e78685c24b011b72d2931d7620c04e0aae47d0ae
enBoig <enboig@gmail.com>
2020-12-18 18:25:23 +0100

    * Added logs on events.

--- app/src/main/java/com/example/mygrids/MainActivity.java
+++ app/src/main/java/com/example/mygrids/MainActivity.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -18,6 +20,9 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Toast.makeText(this, "In onCreate", Toast.LENGTH_SHORT).show();
+        Log.i("info", "In onCreate");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -64,7 +69,42 @@ public class MainActivity extends AppCompatActivity {
             Random rnd = new Random();
             texte.setBackgroundColor(Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256)));
         });
-
-
     }
+
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "In onStart", Toast.LENGTH_SHORT).show();
+        Log.i("m7777", "In onStart");
+    }
+
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "In onResume", Toast.LENGTH_SHORT).show();
+        Log.i("m7777", "In onResume");
+    }
+
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "In onPause", Toast.LENGTH_SHORT).show();
+        Log.i("m7777", "In onPause");
+    }
+
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "In onStop", Toast.LENGTH_SHORT).show();
+        Log.i("m7777", "In onStop");
+    }
+
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "In onRestart", Toast.LENGTH_SHORT).show();
+        Log.i("m7777", "In onRestart");
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "In onDestroy", Toast.LENGTH_SHORT).show();
+        Log.i("m7777", "In onDestroy");
+    }
+
 }
